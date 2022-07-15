bacd478f480af588dbb5ce0df2816b2441ca3a34
ps.sujit.sharma <sujit.sharma@clusus.com>
2022-07-15 13:30:50 +0545

    make spring boot project for poc

---
+++ src/main/java/com/dhakal/pochub/PocHubApplication.java
@@ -0,0 +1,13 @@
+package com.dhakal.pochub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dhakal/pochub/PocHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhakal.pochub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
