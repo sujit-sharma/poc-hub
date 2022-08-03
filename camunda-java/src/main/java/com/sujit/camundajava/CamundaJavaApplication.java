5ef87b34ddc3246822f1b36f6d8b26e3b29c62c2
sujitsharma1166 <sujit.dhakal1166@gmail.com>
2022-08-03 16:15:28 +0545

    feature: camunda spring boot poc implemanted

---
+++ camunda-java/src/main/java/com/sujit/camundajava/CamundaJavaApplication.java
@@ -0,0 +1,27 @@
+package com.sujit.camundajava;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+@EnableProcessApplication
+public class CamundaJavaApplication {
+
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @EventListener
+    private void processPostDeploy(PostDeployEvent event) {
+        runtimeService.startProcessInstanceByKey("loanApproval");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaJavaApplication.class, args);
+    }
+
+}
---
+++ camunda-java/src/test/java/com/sujit/camundajava/CamundaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujit.camundajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
